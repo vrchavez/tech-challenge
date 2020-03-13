624111de1077215c817a716d3e68cca09485c7d4
Ruben Lopez <vrchavez07@gmail.com>
2020-03-13 08:27:36 -0700

    Updates

---
+++ precache-manifest.8904ea59536f3a1e735fab8f808b77bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04964e9508b34aefe1c2eb229cddd58d",
+    "url": "/tech-challenge/index.html"
+  },
+  {
+    "revision": "4a2f86b6ef4b7e91ffdb",
+    "url": "/tech-challenge/static/css/main.81ea6e0b.chunk.css"
+  },
+  {
+    "revision": "2211860aa734c3670307",
+    "url": "/tech-challenge/static/js/2.01d22188.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/tech-challenge/static/js/2.01d22188.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a2f86b6ef4b7e91ffdb",
+    "url": "/tech-challenge/static/js/main.06b72510.chunk.js"
+  },
+  {
+    "revision": "7b0893c5fb13c87293d6",
+    "url": "/tech-challenge/static/js/runtime-main.ae973c9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tech-challenge/static/media/logo.5d5d9eef.svg"
+  }
+]);
